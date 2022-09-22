4e06581925c694b5ea1a938ba5a2eb3554132cb7
DenisShatov <104921777+DenisShatov@users.noreply.github.com>
2022-09-22 22:19:08 +0300

    update

--- Task2.py
+++ Task2.py
@@ -2,7 +2,7 @@ import requests
 
 
 def create_folder(folder):
-    token = 'AQAAAAARJ0BAAADLW4fnBdNTr0m8taVtPKSy6bY'
+    token = '___'
     url = 'https://cloud-api.yandex.net/v1/disk/resources?path=' + folder
     headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {token}'}
     response = requests.put(url, headers=headers)
